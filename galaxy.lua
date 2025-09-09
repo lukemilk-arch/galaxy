5373747ba02ee5a34c21bd20007c12e2996c0667
lukemilk-arch <lukiprawat@gmail.com>
2025-09-10 00:48:22 +0700

    galaxy.lua

--- galaxy.lua
+++ galaxy.lua
@@ -2,7 +2,7 @@ local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/rel
 
 local Window = WindUI:CreateWindow({
     Title = "Galaxy",
-    Icon = "15410082162",
+    Icon = "691443349047535383",
     Author = "Blox Fruits : PREMIUM",
     Size = UDim2.fromOffset(400, 300),
     Transparent = true,
